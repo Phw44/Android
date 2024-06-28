795003ed23dcde44a4cba9d3ece3bb3ddf666dc3
caarmly <camlytran17092003@gmail.com>
2024-06-28 20:56:32 +0700

    change language

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation(libs.firebase.database)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/tlu/cse/ht63/cosmetics/Activity/MainActivity.java
+++ app/src/main/java/tlu/cse/ht63/cosmetics/Activity/MainActivity.java
@@ -1,16 +1,26 @@
 package tlu.cse.ht63.cosmetics.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import java.util.List;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,44 +29,50 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Locale;
 
 import tlu.cse.ht63.cosmetics.Adapter.PopularAdapter;
 import tlu.cse.ht63.cosmetics.Model.ItemsPopularModel;
 import tlu.cse.ht63.cosmetics.R;
+
 import tlu.cse.ht63.cosmetics.databinding.ActivityMainBinding;
 
-public class MainActivity extends BaseActivity {
-
-    private ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity {
     private PopularAdapter adapter;
     private List<ItemsPopularModel> items;
     private List<ItemsPopularModel> originalItems;
     private DatabaseReference database;
     EditText edtSearch;
-    TextView txtExplorer, txtCart, txtProfile;
+    TextView txtExplorer, txtCart, txtProfile, txtProducts;
+    RecyclerView rvItemsProduct;
+    ProgressBar progressBarItemsProduct;
+    ImageView cartBtn, profileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
         items = new ArrayList<>();
         originalItems = new ArrayList<>(); // Initialize the originalItems list
         adapter = new PopularAdapter((ArrayList<ItemsPopularModel>) items);
 
-        binding.rvItemsProduct.setLayoutManager(new GridLayoutManager(this, 2));
-        binding.rvItemsProduct.setAdapter(adapter);
-
         database = FirebaseDatabase.getInstance().getReference();
 
-        initItemsPopular();
         initUI();
-        initLisner();
+        initListener();
+        initItemsPopular();
+        bottomNavigation();
+    }
 
+    private void bottomNavigation() {
+//        cartBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this
+//                , CartActivity.class)));
+        profileBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this
+                , ProfileActivity.class)));
+    }
 
+    private void initListener() {
         edtSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -77,20 +93,10 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-    private void initLisner() {
-        txtProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
     private void initItemsPopular() {
         DatabaseReference myRef = database.child("Items");
 
-        binding.progressBarItemsProduct.setVisibility(View.VISIBLE);
+        progressBarItemsProduct.setVisibility(View.VISIBLE);
 
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -98,6 +104,7 @@ public class MainActivity extends BaseActivity {
                 if (snapshot.exists()) {
                     items.clear();  // Clear the existing items
                     originalItems.clear(); // Clear the original items
+
                     for (DataSnapshot issue : snapshot.getChildren()) {
                         ItemsPopularModel item = issue.getValue(ItemsPopularModel.class);
                         if (item != null) {
@@ -107,7 +114,7 @@ public class MainActivity extends BaseActivity {
                     }
                     adapter.notifyDataSetChanged(); // Notify the adapter of data changes
                 }
-                binding.progressBarItemsProduct.setVisibility(View.GONE);
+                progressBarItemsProduct.setVisibility(View.GONE);
             }
 
             @Override
@@ -127,11 +134,61 @@ public class MainActivity extends BaseActivity {
         adapter.updateList(filteredList);
     }
 
-
     private void initUI() {
+        rvItemsProduct = findViewById(R.id.rvItemsProduct);
         edtSearch = findViewById(R.id.edtSearch);
         txtExplorer = findViewById(R.id.txtExplorer);
         txtCart = findViewById(R.id.txtCart);
         txtProfile = findViewById(R.id.txtProfile);
+        progressBarItemsProduct = findViewById(R.id.progressBarItemsProduct);
+        txtProducts = findViewById(R.id.txtProducts);
+        cartBtn = findViewById(R.id.cartBtn);
+        profileBtn = findViewById(R.id.profileBtn);
+
+        // Set up RecyclerView
+        rvItemsProduct.setLayoutManager(new GridLayoutManager(this, 2));
+        rvItemsProduct.setAdapter(adapter);
     }
+
+
+    // chuyen doi ngon ngu
+    public void loadLocale() {
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        if (!language.isEmpty()) {
+            setLocale(language);
+        }
+    }
+
+    private void setLocale(String langCode) {
+        Locale locale = new Locale(langCode);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", langCode);
+        editor.apply();
+
+        updateTexts(); // Update UI texts directly instead of recreating the activity
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadLocale(); // Kiểm tra và cập nhật ngôn ngữ mỗi khi MainActivity được khởi động lại
+    }
+
+
+    private void updateTexts() {
+        String searchHint = getResources().getString(R.string.search_hint);
+        edtSearch.setHint(searchHint);
+        txtProducts.setText(R.string.txt_products);
+        txtExplorer.setText(R.string.txt_explorer);
+        txtCart.setText(R.string.txt_cart);
+        txtProfile.setText(R.string.txt_profile);
+    }
+
 }
